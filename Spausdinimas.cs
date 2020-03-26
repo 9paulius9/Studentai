256eb57c1e75f91aab614ad31b1236541d79e7cc
9paulius9 <45979522+9paulius9@users.noreply.github.com>
2020-03-26 11:13:16 +0200

    v0.5
    v0.5

--- Spausdinimas.cs
+++ Spausdinimas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace ipaantras
 {
@@ -29,11 +30,32 @@ namespace ipaantras
         public static void rezspausdinimasfailas()
         {
             Console.WriteLine("Vardas".PadRight(10) + "Pavardė".PadRight(10) + "Galutinis(Vid.)/".PadRight(14) + "Galutinis(Med.)");
-            Console.WriteLine("-----------------------------------------------------------------");
-            Studentai.vardasstringlist.Sort();
+            Console.WriteLine("------------------------------------");
+
             for (int i = 0; i < Studentai.studentukiekis; i++)
             {
                 Console.WriteLine(Studentai.vardasstringlist[i].PadRight(9) + Studentai.pavardestringlist[i].PadRight(10) + "".PadRight(11) + $"{Studentai.galutinisdoublelist[i]:0.00}".PadRight(17) + $"{Studentai.galutinismedianadoublelist[i]:0.00}");
+            }
+        }
+
+        public static void rezspausdinimasSuLinkedList()
+        {
+            Console.WriteLine("Vardas".PadRight(10) + "Pavardė".PadRight(10) + "Galutinis(Vid.)/".PadRight(14) + "Galutinis(Med.)");
+            Console.WriteLine("------------------------------------");
+
+            for (int i = 0; i < Studentai.studentukiekis; i++)
+            {
+                Console.WriteLine(Studentai.vardasstringLList.ElementAt(i).PadRight(9) + Studentai.pavardestringLList.ElementAt(i).PadRight(10) + "".PadRight(11) + $"{Studentai.galutinisdoubleLList.ElementAt(i):0.00}".PadRight(17) + $"{Studentai.galutinismedianadoubleLList.ElementAt(i):0.00}");
+            }
+        }
+        public static void rezspausdinimasSuQueue()
+        {
+            Console.WriteLine("Vardas".PadRight(10) + "Pavardė".PadRight(10) + "Galutinis(Vid.)/".PadRight(14) + "Galutinis(Med.)");
+            Console.WriteLine("------------------------------------");
+
+            for (int i = 0; i < Studentai.studentukiekis; i++)
+            {
+                Console.WriteLine(Studentai.vardasstringQueue.ElementAt(i).PadRight(9) + Studentai.pavardestringQueue.ElementAt(i).PadRight(10) + "".PadRight(11) + $"{Studentai.galutinisdoubleQueue.ElementAt(i):0.00}".PadRight(17) + $"{Studentai.galutinismedianadoubleQueue.ElementAt(i):0.00}");
             }
         }
 
@@ -91,5 +113,42 @@ namespace ipaantras
                 Console.WriteLine($"Generic Exception Handler: {e}");
             }
         }
+
+        public static void vienasFailas()
+        {
+            StreamWriter sw = new StreamWriter("D:\\Files\\Darbai(Disk D)\\Programavimas\\ipaantras\\ipaantras\\SugeneruotiSarasai\\v5failas.txt");
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Studentai.suma = 0;
+                Studentai.studentoi = i;
+                Studentai.vardasstringlist.Add("Vardas" + (i + 1));
+                sw.Write("Vardas" + (i + 1));
+                sw.Write(" ");
+                Studentai.pavardestringlist.Add("Pavarde" + (i + 1));
+                sw.Write("Pavarde" + (i + 1));
+                sw.Write(" ");
+                Studentai.ndkiekis = Studentai.ndivertinimasrandom.Next(1, 10);
+                for (int j = 0; j < 5; j++)
+                {
+                    Studentai.ndivertinimas = Studentai.ndivertinimasrandom.Next(1, 10);
+                    sw.Write(Studentai.ndivertinimas);
+                    sw.Write(" ");
+                    Studentai.ndivertimaividinislistas.Add(Studentai.ndivertinimas);
+
+                    Studentai.ndivertinimailist.Add(Studentai.ndivertimaividinislistas);
+
+                    Studentai.suma += Studentai.ndivertinimailist[i][j];
+                }
+
+                Studentai.egzaminoivertinimas = Studentai.ndivertinimasrandom.Next(1, 10);
+                sw.WriteLine(Studentai.egzaminoivertinimas);
+                Studentai.egzaminorezdoublelist.Add(Studentai.egzaminoivertinimas);
+                Galutiniai.galutinisskaiciavimas();
+
+            }
+            sw.Close();
+        }
+
     }
 }
